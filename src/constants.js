aed1b5d1fa7c4a83bd1205efaedbeef5377e9672
Hrithik Anand <hrithik.anand02@gmail.com>
2025-07-11 06:06:09 +0530

    Completed About Section

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const personalInfo = {
+  firstName: "Hrithik",
+  lastName: "Anand",
+  githubLink: "https://github.com/Hrithik25",
+  linkedInLink: "https://www.linkedin.com/in/hrithik25",
+  hackerRankLink: "https://www.hackerrank.com/profile/Hrithik25",
+  resumeLink: "https://drive.google.com/file/d/1Mkf_L6-rJPRgHildqGMht8zj6VC_QIoZ/view?usp=sharing",
+  roles: ["Fullstack Developer", "App Developer", "UI/UX Designer", "Coder"],
+  aboutMe:
+    "I am a full-stack developer with over 2 years of experience in building scalable web applications. Skilled in both front-end and back-end development, I specialize in the MERN stack and other modern technologies to create seamless user experiences and efficient solutions.",
+};
